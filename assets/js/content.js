a2cbd8aaad138e7648b0be6f6e2de693a76af09f
Alexander Mospan <alexander.mospan@gmail.com>
2021-10-12 21:58:11 +0300

    add prettier

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,33 +1,38 @@
-"use strict";
+"use strict"
 
-var app = {};
+var app = {}
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-
-    if (request.method == "set_action") {
-        if (request.key == "click") {
-            sendResponse({
-                status: true,
-                url: location.href
-            });
-        }
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.method == "set_action") {
+    if (request.key == "click") {
+      sendResponse({
+        status: true,
+        url: location.href
+      })
     }
+  }
+})
 
-});
-
-document.addEventListener('contextmenu', function(event) {
-    var node = event.target;
-    while (node && node.nodeName.toLowerCase() != 'a') {
-        if (node.nodeName.toLowerCase() == 'img' && node.parentNode.nodeName.toLowerCase() != 'a') {
-            break;
-        }
-        node = node.parentNode;
+document.addEventListener(
+  "contextmenu",
+  function (event) {
+    var node = event.target
+    while (node && node.nodeName.toLowerCase() != "a") {
+      if (
+        node.nodeName.toLowerCase() == "img" &&
+        node.parentNode.nodeName.toLowerCase() != "a"
+      ) {
+        break
+      }
+      node = node.parentNode
     }
-    app.target = node;
-}, true);
+    app.target = node
+  },
+  true
+)
 
-chrome.extension.onRequest.addListener(function(request, sender, callback) {
-    if (request == 'copy') {
-        callback(app.target.href ? app.target.href : app.target.src);
-    }
-});
+chrome.extension.onRequest.addListener(function (request, sender, callback) {
+  if (request == "copy") {
+    callback(app.target.href ? app.target.href : app.target.src)
+  }
+})
--- assets/js/options.js
+++ assets/js/options.js
@@ -1,25 +1,28 @@
 function setChildTextNode(elementId, text) {
-    document.getElementById(elementId).innerText = text;
+  document.getElementById(elementId).innerText = text
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    setChildTextNode('labelHideMenu', chrome.i18n.getMessage("hide_menu"));
-});
+document.addEventListener("DOMContentLoaded", function () {
+  setChildTextNode("labelHideMenu", chrome.i18n.getMessage("hide_menu"))
+})
 
-chrome.runtime.sendMessage({
-    action: 'getOptions'
-}, function(response) {
-    var options = {};
-    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    Array.prototype.forEach.call(checkboxes, function(checkbox) {
-        var id = checkbox.id;
-        checkbox.checked = options[id] = !!response.settings[id];
-        checkbox.addEventListener('change', function() {
-            options[id] = !!checkbox.checked;
-            chrome.runtime.sendMessage({
-                action: 'setOptions',
-                payload: options
-            });
-        });
-    });
-});
+chrome.runtime.sendMessage(
+  {
+    action: "getOptions"
+  },
+  function (response) {
+    var options = {}
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]')
+    Array.prototype.forEach.call(checkboxes, function (checkbox) {
+      var id = checkbox.id
+      checkbox.checked = options[id] = !!response.settings[id]
+      checkbox.addEventListener("change", function () {
+        options[id] = !!checkbox.checked
+        chrome.runtime.sendMessage({
+          action: "setOptions",
+          payload: options
+        })
+      })
+    })
+  }
+)
