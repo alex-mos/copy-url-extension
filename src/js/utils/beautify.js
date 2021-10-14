f62f0885adbdc6abc4e996d47f980460580cabea
Alexander Mospan <alexander.mospan@gmail.com>
2021-10-14 18:19:49 +0300

    refactor beautify

--- src/js/utils/beautify.js
+++ src/js/utils/beautify.js
@@ -1,22 +1,8 @@
 const punycode = require("punycode")
 
-module.exports = function beautify(url) {
-  console.log("url")
-  console.log(url)
-  url = decodeURIComponent(url).replace(/\s/g, "%20")
-  let a
-  if (url.search("http://") != -1) {
-    a = 7
-  } else {
-    a = 8
-  }
-  let domain = url.substring(a)
-  domain = domain.split("/")
-  console.log("domain[0]")
-  console.log(domain[0])
-  const result = punycode.toUnicode(domain[0])
-  console.log("result")
-  console.log(result)
-  url = url.replace(domain[0], result)
-  return url
+module.exports = function beautify(urlString) {
+  const url = new URL(urlString)
+  const decodedHostname = punycode.toUnicode(url.hostname)
+  const decodedPathname = decodeURIComponent(url.pathname).replace(/\s/g, "%20")
+  return `${url.protocol}//${decodedHostname}${decodedPathname}`
 }
--- tests/beautify.test.js
+++ tests/beautify.test.js
@@ -7,3 +7,19 @@ test("https://ru.wikipedia.org/wiki/Московский_зоопарк", () => 
     )
   ).toBe("https://ru.wikipedia.org/wiki/Московский_зоопарк")
 })
+
+test("https://ru.wikipedia.org/wiki/Московский зоопарк", () => {
+  expect(
+    beautify(
+      "https://ru.wikipedia.org/wiki/%D0%9C%D0%BE%D1%81%D0%BA%D0%BE%D0%B2%D1%81%D0%BA%D0%B8%D0%B9%20%D0%B7%D0%BE%D0%BE%D0%BF%D0%B0%D1%80%D0%BA"
+    )
+  ).toBe("https://ru.wikipedia.org/wiki/Московский%20зоопарк")
+})
+
+test("http://вещай.рф/клуб любителей английского", () => {
+  expect(
+    beautify(
+      "http://xn--80adjr2f.xn--p1ai/%D0%BA%D0%BB%D1%83%D0%B1%20%D0%BB%D1%8E%D0%B1%D0%B8%D1%82%D0%B5%D0%BB%D0%B5%D0%B9%20%D0%B0%D0%BD%D0%B3%D0%BB%D0%B8%D0%B9%D1%81%D0%BA%D0%BE%D0%B3%D0%BE"
+    )
+  ).toBe("http://вещай.рф/клуб%20любителей%20английского")
+})
